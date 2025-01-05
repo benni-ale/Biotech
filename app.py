9d0082b191adbe41ceae9bdf631dee8f37146bb9
Alessandro Beninati <abeninati@gmail.com>
2025-01-05 16:08:17 +0100

    All companies not only biotech

--- app.py
+++ app.py
@@ -5,10 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Elenco di aziende biotecnologiche e farmaceutiche
-    companies = ['AMGN', 'GILD', 'BIIB', 'VRTX', 'REGN']  # Esempi di ticker per aziende biotech/farmaceutiche
+    # Elenco di tutte le aziende
+    companies = ['AMGN', 'GILD', 'BIIB', 'VRTX', 'REGN', 'AAPL', 'MSFT', 'GOOGL']  # Esempi di ticker per aziende
     data = {company: yf.Ticker(company).info for company in companies}
     return render_template('index.html', data=data)
+
+@app.route('/biotech')
+def biotech():
+    # Elenco di aziende biotecnologiche
+    biotech_companies = ['AMGN', 'GILD', 'BIIB', 'VRTX', 'REGN']  # Esempi di ticker per aziende biotech
+    data = {company: yf.Ticker(company).info for company in biotech_companies}
+    return render_template('biotech.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True) 
