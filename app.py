b6926976e57b643ed7ffe12263d80704d903dc99
Alessandro Beninati <abeninati@gmail.com>
2025-01-05 12:56:13 +0100

    Third commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import yfinance as yf
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # Elenco di aziende biotecnologiche e farmaceutiche
+    companies = ['AMGN', 'GILD', 'BIIB', 'VRTX', 'REGN']  # Esempi di ticker per aziende biotech/farmaceutiche
+    data = {company: yf.Ticker(company).info for company in companies}
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True) 
